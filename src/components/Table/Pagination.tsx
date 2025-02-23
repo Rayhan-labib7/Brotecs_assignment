767a8615e7d300c7e9b8bf9799a015145ae48f22
Jahir Rayhan <jahir.rayhan@bedatasolutions.com>
2025-02-23 16:28:59 +0600

    responsive

--- src/components/Table/Pagination.tsx
+++ src/components/Table/Pagination.tsx
@@ -20,13 +20,12 @@ const Pagination: React.FC<PaginationProps> = ({
   handlePageChange,
   handleRowsPerPageChange,
 }) => {
-  // Rows per page
   const pageSize = [2, 5, 10, 20, 50];
   const { isDarkMode } = useTheme();
 
   const generatePageNumbers = (): (string | number)[] => {
     const pageNumbers: (string | number)[] = [];
-    const maxVisiblePages = 2; // Number of pages to show around the current page
+    const maxVisiblePages = 2;
     const leftFlag = currentPage - maxVisiblePages;
     const rightFlag = currentPage + maxVisiblePages;
     const startPage = Math.max(2, leftFlag);
@@ -34,15 +33,10 @@ const Pagination: React.FC<PaginationProps> = ({
 
     pageNumbers.push(1);
 
-    // Add '...' if there are pages before the visible range
     if (leftFlag > 2) pageNumbers.push('...');
-
-    // Add pages around the current page
     for (let i: number = startPage; i <= endPage; i += 1) {
       pageNumbers.push(i);
     }
-
-    // Add '...' if there are pages after the visible range
     if (rightFlag < totalPages - 1) pageNumbers.push('...');
     if (totalPages > 1) pageNumbers.push(totalPages);
 
@@ -69,99 +63,99 @@ const Pagination: React.FC<PaginationProps> = ({
     );
 
   return (
-    <div className={cn('flex justify-between p-4 border-t', isDarkMode ? 'border-gray-600 bg-gray-800' : 'border-gray-200 bg-white')}>
-    {/* Inputs row */}
-    <div className="flex flex-col sm:flex-row  space-x-5 items-center mb-4 sm:mb-0">
-      {/* Rows per page */}
-      <div className="flex items-center mb-2 sm:mb-0">
-        <span className={cn('mr-2 text-xs', isDarkMode ? 'text-gray-300' : 'text-brotecs-black-2')}>Rows per page:</span>
-        <select
-          value={rowsPerPage}
-          onChange={handleRowsPerPageChange}
+    <div className={cn('flex flex-col sm:flex-row justify-between p-4 border-t', isDarkMode ? 'border-gray-600 bg-gray-800' : 'border-gray-200 bg-white')}>
+      {/* Inputs column */}
+      <div className="flex flex-col sm:flex-row space-y-2 sm:space-y-0 sm:space-x-5 items-center sm:mb-0">
+        {/* Rows per page */}
+        <div className="flex items-center">
+          <span className={cn('mr-2 text-xs', isDarkMode ? 'text-gray-300' : 'text-brotecs-black-2')}>Rows per page:</span>
+          <select
+            value={rowsPerPage}
+            onChange={handleRowsPerPageChange}
+            className={cn(
+              'border rounded-md px-1 py-1 w-12 transition-colors',
+              isDarkMode ? 'bg-gray-700 border-gray-600 text-gray-300' : 'border-brotecs-black-1'
+            )}
+          >
+            {pageSize.map((rows) => (
+              <option key={rows} value={rows}>
+                {rows}
+              </option>
+            ))}
+          </select>
+        </div>
+
+        {/* Go to page */}
+        <div className="flex items-center">
+          <span className={cn('mr-2 text-xs', isDarkMode ? 'text-gray-300' : 'text-brotecs-black-2')}>Go to</span>
+          <input
+            type="number"
+            min="1"
+            max={totalPages}
+            value={currentPage}
+            onChange={(e) => handlePageChange(Number(e.target.value))}
+            className={cn(
+              'border rounded-md w-12 px-2 py-1 transition-colors',
+              isDarkMode ? 'bg-gray-700 border-gray-600 text-gray-300' : 'border-brotecs-black-1'
+            )}
+          />
+        </div>
+      </div>
+
+      {/* Buttons column */}
+      <div className="flex justify-center sm:justify-end mt-2 sm:mt-0">
+        {/* Pagination buttons */}
+        <Button
+          onClick={() => handlePageChange(1)}
+          disabled={currentPage === 1}
           className={cn(
-            'border rounded-md px-1 py-1 w-12 transition-colors',
-            isDarkMode ? 'bg-gray-700 border-gray-600 text-gray-300' : 'border-brotecs-black-1'
+            'border mr-2 transition-colors',
+            isDarkMode ? 'border-gray-600 bg-gray-800 text-gray-300 hover:bg-gray-700' : 'border-gray-300 hover:bg-gray-50'
           )}
+          size="sm"
         >
-          {pageSize.map((rows) => (
-            <option key={rows} value={rows}>
-              {rows}
-            </option>
-          ))}
-        </select>
-      </div>
-  
-      {/* Go to page */}
-      <div className="flex items-center">
-        <span className={cn('mr-2 text-xs', isDarkMode ? 'text-gray-300' : 'text-brotecs-black-2')}>Go to</span>
-        <input
-          type="number"
-          min="1"
-          max={totalPages}
-          value={currentPage}
-          onChange={(e) => handlePageChange(Number(e.target.value))}
+          <span dangerouslySetInnerHTML={{ __html: svgIcons.previousBtn }} />
+        </Button>
+
+        <Button
+          onClick={() => handlePageChange(currentPage - 1)}
+          disabled={currentPage === 1}
           className={cn(
-            'border rounded-md w-12 px-2 py-1 transition-colors',
-            isDarkMode ? 'bg-gray-700 border-gray-600 text-gray-300' : 'border-brotecs-black-1'
+            'border transition-colors',
+            isDarkMode ? 'border-gray-600 bg-gray-800 text-gray-300 hover:bg-gray-700' : 'border-gray-300 hover:bg-gray-50'
           )}
-        />
+          size="sm"
+        >
+          <ArrowLeft2 size="16" color={isDarkMode ? '#D1D5DB' : '#566573'} />
+        </Button>
+
+        {generatePageNumbers().map(renderPageButton)}
+
+        <Button
+          onClick={() => handlePageChange(currentPage + 1)}
+          disabled={currentPage === totalPages}
+          className={cn(
+            'border transition-colors',
+            isDarkMode ? 'border-gray-600 bg-gray-800 text-gray-300 hover:bg-gray-700' : 'border-gray-300 hover:bg-gray-50'
+          )}
+          size="sm"
+        >
+          <ArrowRight2 size="16" color={isDarkMode ? '#D1D5DB' : '#566573'} />
+        </Button>
+
+        <Button
+          onClick={() => handlePageChange(totalPages)}
+          disabled={currentPage === totalPages}
+          className={cn(
+            'border ml-2 transition-colors',
+            isDarkMode ? 'border-gray-600 bg-gray-800 text-gray-300 hover:bg-gray-700' : 'border-gray-300 hover:bg-gray-50'
+          )}
+          size="sm"
+        >
+          <span dangerouslySetInnerHTML={{ __html: svgIcons.forwardBtn }} />
+        </Button>
       </div>
     </div>
-  
-    {/* Buttons row */}
-    <div className="flex justify-center sm:justify-end">
-      {/* Pagination buttons */}
-      <Button
-        onClick={() => handlePageChange(1)}
-        disabled={currentPage === 1}
-        className={cn(
-          'border mr-2 transition-colors',
-          isDarkMode ? 'border-gray-600 bg-gray-800 text-gray-300 hover:bg-gray-700' : 'border-gray-300 hover:bg-gray-50'
-        )}
-        size="sm"
-      >
-        <span dangerouslySetInnerHTML={{ __html: svgIcons.previousBtn }} />
-      </Button>
-  
-      <Button
-        onClick={() => handlePageChange(currentPage - 1)}
-        disabled={currentPage === 1}
-        className={cn(
-          'border transition-colors',
-          isDarkMode ? 'border-gray-600 bg-gray-800 text-gray-300 hover:bg-gray-700' : 'border-gray-300 hover:bg-gray-50'
-        )}
-        size="sm"
-      >
-        <ArrowLeft2 size="16" color={isDarkMode ? '#D1D5DB' : '#566573'} />
-      </Button>
-  
-      {generatePageNumbers().map(renderPageButton)}
-  
-      <Button
-        onClick={() => handlePageChange(currentPage + 1)}
-        disabled={currentPage === totalPages}
-        className={cn(
-          'border transition-colors',
-          isDarkMode ? 'border-gray-600 bg-gray-800 text-gray-300 hover:bg-gray-700' : 'border-gray-300 hover:bg-gray-50'
-        )}
-        size="sm"
-      >
-        <ArrowRight2 size="16" color={isDarkMode ? '#D1D5DB' : '#566573'} />
-      </Button>
-  
-      <Button
-        onClick={() => handlePageChange(totalPages)}
-        disabled={currentPage === totalPages}
-        className={cn(
-          'border ml-2 transition-colors',
-          isDarkMode ? 'border-gray-600 bg-gray-800 text-gray-300 hover:bg-gray-700' : 'border-gray-300 hover:bg-gray-50'
-        )}
-        size="sm"
-      >
-        <span dangerouslySetInnerHTML={{ __html: svgIcons.forwardBtn }} />
-      </Button>
-    </div>
-  </div>
   );
 };
 
